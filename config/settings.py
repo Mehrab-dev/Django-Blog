e49e96287b86838d8635e9250ce145ccd8d58bfb
Mehrab <programmer.py.mail@gmail.com>
2025-11-17 01:29:47 +0330

    created home page

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'statics']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media'
