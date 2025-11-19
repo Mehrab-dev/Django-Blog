0e273c15618eda83f326d00627ea23925ab0ff22
Mehrab <programmer.py.mail@gmail.com>
2025-11-19 03:55:14 +0330

    created Post and Category models for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post , Category
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin) :
+    date_hierarchy = 'created_date'
+    list_display = ['id','title','author','published_date','counted_view','status']
+    list_filter = ['status','author']
+    search_fields = ['author']
+    list_display_links = ['id','title']
+
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model) :
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model) :
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    published_date = models.DateField(null=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    image = models.ImageField(upload_to='blog/',null=True)
+    category = models.ManyToManyField(Category)
+    status = models.BooleanField(default=True)
+    counted_view = models.IntegerField(default=0)
+    
+    class Meta :
+        ordering = ['created_date']
+
+    def __str__(self) :
+        return '{} - {}'.format(self.id,self.title)
