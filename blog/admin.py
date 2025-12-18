530b006d22b25b0e46e36e1509724ce6617df8a1
Mehrab <programmer.py.mail@gmail.com>
2025-12-19 01:27:55 +0330

    creating Post , Category models in blog.models and creating PostAdmin in blog.admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post , Category
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin) :
+    list_display = ['title','author','status','created_date','published_date']
+    list_filter = ['status','author']
+    search_fields = ['status','author']
+
+admin.site.register(Category)
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# create models
+class Post(models.Model) :
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null=True)
+    content = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
+    status = models.BooleanField(default=True)
+    counted_view = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self) :
+        return self.title
+
+
+class Category(models.Model) :
+    name = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.name
+    
